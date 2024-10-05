0a3338ac170f55008872b2dd080df4184f32047f
oatlpk <oat9837@gmail.com>
2024-10-06 00:50:20 +0700

    Add generic button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
 export default function Home() {
   return (
-    // <div className="flex min-h-screen items-center justify-center">
-    //   <Image
-    //     className="invert"
-    //     src="https://nextjs.org/icons/next.svg"
-    //     alt="Next.js logo"
-    //     width={180}
-    //     height={38}
-    //     priority
-    //   />
-    // </div>
-    <div className="min-h-screen"></div>
+    <div className="flex min-h-screen w-full items-center justify-center"></div>
   );
 }
