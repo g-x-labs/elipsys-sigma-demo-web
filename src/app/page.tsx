993535a83d4de1e286b86d81b14787717a0e5a6e
oatlpk <oat9837@gmail.com>
2024-10-07 00:20:38 +0700

    Adjust card component position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import SwapHeader from "@/components/home/swap/SwapHeader";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center">
-      <div className="flex w-[456px] flex-col gap-y-6 p-4">
+    <div className="flex h-full min-h-screen w-full justify-center overflow-x-hidden">
+      <div className="mx-auto flex w-[456px] flex-col gap-y-6 p-4 pt-[134px]">
         <SwapHeader />
         <SwapCard />
       </div>
