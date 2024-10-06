fa8009e619355594a4d3096df1f0c785fbd6e4ab
oatlpk <oat9837@gmail.com>
2024-10-06 16:30:58 +0700

    Add swap header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import SwapHeader from "@/components/home/Header";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center"></div>
+    <div className="flex min-h-screen w-full items-center justify-center">
+      {/* Swap Component */}
+      <div className="flex w-[456px] gap-x-6 p-4">
+        <SwapHeader />
+        {/* TokenInput */}
+        {/* Cost Estimation */}
+        {/* Button */}
+      </div>
+    </div>
   );
 }
