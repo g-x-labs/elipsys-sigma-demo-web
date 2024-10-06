7c6fa9eb9f14e97172dcd49e8ff17589dd802276
oatlpk <oat9837@gmail.com>
2024-10-06 15:48:55 +0700

    Add not found page

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/shared/Button";
 export default function Navbar() {
   return (
     <nav className="fixed left-0 top-0 z-50 flex w-full items-center justify-between px-16 py-8">
-      {/* Left side of the Navbar */}
+      {/* TODO: Consider linking back to home */}
       <ElipsysIcon className="w-[96px]" />
 
       {/* TODO: Link wallet connet */}
