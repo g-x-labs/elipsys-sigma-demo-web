ff968b0032bb839672ec9a93694a983bee268ba6
oatlpk <oat9837@gmail.com>
2024-10-07 19:43:13 +0700

    Style network and token columns

--- src/components/modals/TokenNetworkModal.tsx
+++ src/components/modals/TokenNetworkModal.tsx
@@ -3,30 +3,52 @@ import Modal from "@/components/ui/Modal";
 export default function TokenNetworkModal() {
   return (
     <Modal modalId="tokenNetworkModal" title="Select">
-      {/* Search bar */}
-      <div className="flex h-96 flex-wrap gap-3 border-t border-gray-800 py-4">
-        {/* Networks Column */}
-        <div className="flex max-w-[172px] flex-1 flex-col gap-y-3 overflow-y-auto border-r border-gray-800">
-          <h3 className="text-gray-400 text-sb3">Networks</h3>
-          <div className="space-y-2">
-            <div className="flex w-[160px] flex-row items-center justify-start gap-x-2 px-3 py-2">
-              <div className="h-8 w-8 rounded-lg bg-red"></div>
-              <span className="text-b3">Chain name</span>
-            </div>
+      {/* TODO: Add search bar */}
+      <div className="flex h-96 w-full gap-3 border-t border-gray-800 pb-4">
+        <div className="flex w-[172px] flex-col gap-y-3 overflow-y-auto border-r border-gray-800">
+          <h3 className="pt-4 text-gray-400 text-sb3">Networks</h3>
+          <div>
+            {Array.from({ length: 10 }).map((_, index) => (
+              <ChainPill key={index} />
+            ))}
           </div>
         </div>
-
-        {/* Tokens Column */}
-        <div className="flex max-w-[252px] flex-1 flex-col gap-y-3 overflow-y-auto">
-          <h3 className="text-gray-400 text-sb3">Tokens</h3>
-          <div className="space-y-2">
-            <div className="flex w-[240px] flex-row items-center justify-start gap-x-2 px-3 py-2">
-              <div className="h-8 w-8 rounded-lg bg-red"></div>
-              <span className="text-b3">Token name</span>
-            </div>
+        <div className="flex w-[252px] flex-col gap-y-3 overflow-y-auto">
+          <h3 className="pt-4 text-gray-400 text-sb3">Tokens</h3>
+          <div>
+            {Array.from({ length: 10 }).map((_, index) => (
+              <TokenPill key={index} />
+            ))}
           </div>
         </div>
       </div>
     </Modal>
+  );
+}
+
+// ChainPill component
+function ChainPill() {
+  return (
+    <div className="flex w-full flex-row items-center justify-start gap-x-2 px-3 py-2">
+      <div className="h-8 w-8 rounded-lg bg-gray-700"></div>
+      <span className="text-gray-400 text-b3">Chain name</span>
+    </div>
+  );
+}
+
+// TokenPill component
+function TokenPill() {
+  return (
+    <div className="flex w-full flex-row items-center justify-start gap-x-2 px-3 py-2">
+      <div className="relative w-[44px]">
+        <div className="h-9 w-9 rounded-full bg-gray-700"></div>
+
+        <div className="absolute bottom-0 right-0 h-[18px] w-[18px] rounded-sm bg-gray-600"></div>
+      </div>
+      <div className="flex flex-col gap-y-[6px]">
+        <span className="text-gray-600 text-sb3">Token Name</span>
+        <span className="uppercase text-gray-400 text-b3">TICKER</span>
+      </div>
+    </div>
   );
 }
