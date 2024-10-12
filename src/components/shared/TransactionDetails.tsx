0880e16ca939b0ef894601d873d58560c04bc7db
oatlpk <oat9837@gmail.com>
2024-10-12 21:45:47 +0700

    Remove unecessary separeted props file

--- src/components/shared/TransactionDetails.tsx
+++ src/components/shared/TransactionDetails.tsx
@@ -1,7 +1,13 @@
 import Tooltip from "@/components/ui/Tooltip";
 import InfoIcon from "@/assets/icons/info.svg";
 import RightArrowIcon from "@/assets/icons/right-arrow.svg";
-import { TransactionDetailProps } from "@/types/home/TransactionDetailsProps";
+
+export interface TransactionDetailProps {
+  label: string;
+  value: string;
+  secondaryValue?: string;
+  tooltip?: string;
+}
 
 // TODO: Reconsider where to place this
 // INFO: This is used in the main component and in the transaction modals
