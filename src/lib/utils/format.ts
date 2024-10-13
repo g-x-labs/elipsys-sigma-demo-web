749c6c3b1b18796bfc00017914bb42afccdabc01
oatlpk <oat9837@gmail.com>
2024-10-13 15:32:23 +0700

    Add formatEstimateTime function

--- src/lib/utils/format.ts
+++ src/lib/utils/format.ts
@@ -23,3 +23,27 @@ export function formatAsUsd(value: number | undefined | null): string {
     maximumFractionDigits: 2,
   }).format(value)}`;
 }
+
+// INFO: Unused for now
+export function formatEstimateTime(seconds: number): string {
+  if (
+    seconds === undefined ||
+    seconds === null ||
+    isNaN(seconds) ||
+    seconds < 0
+  ) {
+    return "--";
+  }
+
+  if (seconds < 60) {
+    return `~${seconds} secs`;
+  }
+
+  const mins = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+
+  const formattedMins = mins.toString().padStart(2, "0");
+  const formattedSecs = secs.toString().padStart(2, "0");
+
+  return `~${formattedMins}:${formattedSecs} mins`;
+}
