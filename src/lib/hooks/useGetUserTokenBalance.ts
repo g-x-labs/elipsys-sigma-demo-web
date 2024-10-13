1ef3deb089892f9ceccc29b0daa5d0781531ec44
Wayu Bangkamed <wayu.bangkamed@gmail.com>
2024-10-13 12:10:13 +0700

    add chainId to balance fetching hook

--- src/lib/hooks/useGetUserTokenBalance.ts
+++ src/lib/hooks/useGetUserTokenBalance.ts
@@ -3,29 +3,35 @@ import { Address } from "viem";
 import { useBalance, useReadContract } from "wagmi";
 import { ierc20Abi } from "@/contract/abi";
 import BigNumber from "bignumber.js";
+import { ChainId } from "@/types/utils";
 /**
  * Description: Retrieves the balance of a specified ERC-20 token for a given address.
  *
  * @function
  * @param {Address} tokenAddress - The address for ERC-20 token
  * @param {Address} address - The wallet address to check the balance for
+ * @param {ChainId} chainId - The chain id to check the balance for
  * @returns {BigNumber | null} - The token balance
  */
 
 export default function useGetUserTokenBalance({
   tokenAddress,
   address,
+  chainId,
 }: {
   tokenAddress: Address | undefined;
   address: Address | undefined;
+  chainId: ChainId;
 }) {
   // INFO: fetch native eth token balance
   const balance = useBalance({
-    address: tokenAddress,
+    address: address,
+    chainId,
   });
 
   const result = useReadContract({
     abi: ierc20Abi,
+    chainId,
     address: tokenAddress,
     functionName: "balanceOf",
     args: [address!],
@@ -35,10 +41,10 @@ export default function useGetUserTokenBalance({
   });
 
   if (tokenAddress === ZERO_ADDRESS) {
-    return balance.data?.value
+    return balance.data !== undefined
       ? BigNumber(balance.data?.value.toString())
       : null;
   } else {
-    return result.data ? BigNumber(result.data.toString()) : null;
+    return result.data !== undefined ? BigNumber(result.data.toString()) : null;
   }
 }
