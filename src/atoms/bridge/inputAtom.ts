1be5aee8cf47dc6820c308ef0e92e3b8df5b42b5
Wayu Bangkamed <wayu.bangkamed@gmail.com>
2024-10-16 13:53:27 +0700

    add comment

--- src/atoms/bridge/inputAtom.ts
+++ src/atoms/bridge/inputAtom.ts
@@ -4,3 +4,5 @@ import { atom } from "jotai";
 // REFACTOR: might consider implement the custom setter/getter function so that we can clean the input with `tokenAmountInputFilter` when setting
 //  and return BigNumber when getting
 export const tokenInputAtom = atom<string>("");
+
+// TODO: implement derived atom for token output
