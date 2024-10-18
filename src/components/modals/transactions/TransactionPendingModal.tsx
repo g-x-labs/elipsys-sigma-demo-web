154a21bec99138c542321d6d7fc8defc6bdac387
oatlpk <79736309+oatlpk@users.noreply.github.com>
2024-10-17 23:39:46 -0700

    [feat] Add Loading Spinner In Pending Modal (#26)
    * Add loading spinner to pending modal
    
    * Fix vertical line in pending and success modal
    
    * Replace height prop with classname in TransactionDivider
    
    * Remove todo comment
    
    * Move classname props behind

--- src/components/modals/transactions/TransactionPendingModal.tsx
+++ src/components/modals/transactions/TransactionPendingModal.tsx
@@ -1,4 +1,5 @@
 import {
+  PerpetualCircularLoader,
   TokenSummary,
   TransactionDetails,
   TransactionDivider,
@@ -31,7 +32,9 @@ const TransactionPendingModal: React.FC = () => {
       modalId={ModalIds.TransactionPendingModal}
       title="Transaction Pending"
     >
-      {/* TODO: Spinner animation */}
+      <div className="flex items-center justify-center py-1">
+        <PerpetualCircularLoader /> {/* Pass duration as needed */}
+      </div>
       <div className="flex w-full flex-col gap-y-5 rounded-lg border border-gray-800 p-4">
         <h2 className="text-gray-400 text-sb3">Entering the Portal</h2>
         <div className="flex w-full flex-col">
@@ -42,7 +45,10 @@ const TransactionPendingModal: React.FC = () => {
             network={fromNetwork}
             destinationAddress={address}
           />
-          <TransactionDivider label="Wormhole (ZK-Threshold)" height="60px" />
+          <TransactionDivider
+            label="Wormhole (ZK-Threshold)"
+            className="h-[60px]"
+          />
           <TokenSummary
             token={toToken}
             tokenAmount={tempTokenAmount}
