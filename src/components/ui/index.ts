438a6b607049c1b09a3d3b964ab258ac8a974a65
oatlpk <oat9837@gmail.com>
2024-10-19 01:10:19 +0700

    Add Select component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -12,3 +12,15 @@ export { Modal } from "./Modal";
 export { Dialog } from "./Dialog";
 export { SearchBar } from "./Search";
 export { Tooltip } from "./Tooltip";
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+} from "./Select";
